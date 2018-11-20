a3d36e284e310befc47213cb23177652107cf435
Per Arnborg <kontakt@perarnborg.se>
2018-11-20 18:51:30 +0100

    Add test with loadUser stub

--- test/create-store-module.test.js
+++ test/create-store-module.test.js
@@ -1,13 +1,20 @@
 const assert = require('assert');
 const sinon = require('sinon');
+const sinonSandbox = sinon.sandbox.create();
 const oidcConfig = require('./oidcTestConfig');
 let vuexOidc;
 let storeModule;
+let UserManager;
 
 describe('createStoreModule', function() {
   before(function () {
     vuexOidc = require('../dist/vuex-oidc.cjs');
     storeModule = vuexOidc.vuexOidcCreateStoreModule(oidcConfig, {dispatchEventsOnWindow: true});
+    UserManager = require('oidc-client').UserManager;
+  });
+
+  afterEach(function () {
+    sinonSandbox.restore();
   });
 
   it('factory function should return a vuex module object', function() {
@@ -19,47 +26,68 @@ describe('createStoreModule', function() {
   });
 
   describe('.actions.oidcCheckAccess', function() {
-    it('should resolve true for public routes if authenticated, and also signout user if state is not in storage', function() {
-      const context = authenticatedContext();
-      sinon.spy(context, 'commit');
-      return storeModule.actions.oidcCheckAccess(context, publicRoute())
-        .then(function(hasAccess) {
-          assert.equal(hasAccess, true);
-          assert.equal(context.commit.calledWith('unsetOidcAuth'), true);
-          context.commit.restore();
-        })
+    describe('check public routes', function() {
+      it('should resolve true for public routes if authenticated', function() {
+        const context = authenticatedContext();
+        return storeModule.actions.oidcCheckAccess(context, publicRoute())
+          .then(function(hasAccess) {
+            assert.equal(hasAccess, true);
+          })
+      });
+      it('should resolve true for public routes if not authenticated, and also dispatch silent auth action', function() {
+        const context = unAuthenticatedContext();
+        sinon.spy(context, 'dispatch');
+        return storeModule.actions.oidcCheckAccess(context, publicRoute())
+          .then(function(hasAccess) {
+            assert.equal(hasAccess, true);
+            assert.equal(context.dispatch.calledWith('authenticateOidcSilent'), true);
+            context.dispatch.restore();
+          })
+      });
     });
-    it('should resolve true for public routes if not authenticated, and also dispatch silent auth action', function() {
-      const context = unAuthenticatedContext();
-      sinon.spy(context, 'dispatch');
-      return storeModule.actions.oidcCheckAccess(context, publicRoute())
-        .then(function(hasAccess) {
-          assert.equal(hasAccess, true);
-          assert.equal(context.dispatch.calledWith('authenticateOidcSilent'), true);
-          context.dispatch.restore();
-        })
+    describe('check callback routes', function() {
+      it('should resolve true for oidcCallbackRoutes routes if authenticated', function() {
+        return storeModule.actions.oidcCheckAccess(authenticatedContext(), oidcCallbackRoute())
+          .then(function(hasAccess) {
+            assert.equal(hasAccess, true);
+          })
+      });
+      it('should resolve true for oidcCallbackRoutes routes if not authenticated', function() {
+        return storeModule.actions.oidcCheckAccess(unAuthenticatedContext(), oidcCallbackRoute())
+          .then(function(hasAccess) {
+            assert.equal(hasAccess, true);
+          })
+      });
     });
-    it('should resolve true for oidcCallbackRoutes routes if authenticated', function() {
-      return storeModule.actions.oidcCheckAccess(authenticatedContext(), oidcCallbackRoute())
-        .then(function(hasAccess) {
-          assert.equal(hasAccess, true);
-        })
-    });
-    it('should resolve true for oidcCallbackRoutes routes if not authenticated', function() {
-      return storeModule.actions.oidcCheckAccess(unAuthenticatedContext(), oidcCallbackRoute())
-        .then(function(hasAccess) {
-          assert.equal(hasAccess, true);
-        })
-    });
-    it('should resolve false for protected routes if not authenticated, and also dispatch auth redirect action', function() {
-      const context = unAuthenticatedContext();
-      sinon.spy(context, 'dispatch');
-      return storeModule.actions.oidcCheckAccess(context, protectedRoute())
-        .then(function(hasAccess) {
-          assert.equal(hasAccess, false);
-          assert.equal(context.dispatch.calledWith('authenticateOidc'), true);
-          context.dispatch.restore();
-        })
+    describe('check protected routes', function() {
+      it('should resolve false for protected routes if not authenticated, and also dispatch auth redirect action', function() {
+        const context = unAuthenticatedContext();
+        sinon.spy(context, 'dispatch');
+        return storeModule.actions.oidcCheckAccess(context, protectedRoute())
+          .then(function(hasAccess) {
+            assert.equal(hasAccess, false);
+            assert.equal(context.dispatch.calledWith('authenticateOidc'), true);
+            context.dispatch.restore();
+          })
+      });
+      it('should resolve true for protected routes if authenticated in both vuex and in storage', function() {
+        const context = authenticatedContext();
+        sinonSandbox.stub(UserManager.prototype, 'getUser').callsFake(getUserPromise);
+        return storeModule.actions.oidcCheckAccess(context, protectedRoute())
+          .then(function(hasAccess) {
+            assert.equal(hasAccess, true);
+          })
+      });
+      it('should resolve false for protected routes if authenticated in vuex, but not in storage. Also signout user from vuex.', function() {
+        const context = authenticatedContext();
+        sinon.spy(context, 'commit');
+        return storeModule.actions.oidcCheckAccess(context, protectedRoute())
+          .then(function(hasAccess) {
+            assert.equal(hasAccess, false);
+            assert.equal(context.commit.calledWith('unsetOidcAuth'), true);
+            context.commit.restore();
+          })
+      });
     });
   });
 
@@ -129,6 +157,12 @@ function protectedRoute() {
     meta: {
     }
   }
+}
+
+function getUserPromise() {
+  return new Promise(function(resolve) {
+    resolve(oidcUser());
+  });
 }
 
 function oidcUser() {
