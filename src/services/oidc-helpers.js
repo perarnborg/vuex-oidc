7ef95bbe8a6a54f1053e5f405dafaaa40aa70969
Pavel Andreev <pavel@andreev.ws>
2020-08-26 11:54:40 +1000

    lint modification reverted

--- src/services/oidc-helpers.js
+++ src/services/oidc-helpers.js
@@ -2,143 +2,147 @@ import {
   objectAssign,
   parseJwt,
   firstLetterUppercase,
-  camelCaseToSnakeCase
-} from './utils'
-import oidc from 'oidc-client'
+  camelCaseToSnakeCase,
+} from "./utils";
+import oidc from "oidc-client";
 
 const defaultOidcConfig = {
   userStore: new oidc.WebStorageStateStore(),
   loadUserInfo: true,
-  automaticSilentSignin: true
-}
+  automaticSilentSignin: true,
+};
 
 const requiredConfigProperties = [
-  'authority',
-  'client_id',
-  'redirect_uri',
-  'response_type',
-  'scope'
-]
+  "authority",
+  "client_id",
+  "redirect_uri",
+  "response_type",
+  "scope",
+];
 
 const settingsThatAreSnakeCasedInOidcClient = [
-  'clientId',
-  'redirectUri',
-  'responseType',
-  'maxAge',
-  'uiLocales',
-  'loginHint',
-  'acrValues',
-  'postLogoutRedirectUri',
-  'popupRedirectUri',
-  'silentRedirectUri'
-]
+  "clientId",
+  "redirectUri",
+  "responseType",
+  "maxAge",
+  "uiLocales",
+  "loginHint",
+  "acrValues",
+  "postLogoutRedirectUri",
+  "popupRedirectUri",
+  "silentRedirectUri",
+];
 
 const snakeCasedSettings = (oidcSettings) => {
   settingsThatAreSnakeCasedInOidcClient.forEach((setting) => {
-    if (typeof oidcSettings[setting] !== 'undefined') {
-      oidcSettings[camelCaseToSnakeCase(setting)] = oidcSettings[setting]
+    if (typeof oidcSettings[setting] !== "undefined") {
+      oidcSettings[camelCaseToSnakeCase(setting)] = oidcSettings[setting];
     }
-  })
-  return oidcSettings
-}
+  });
+  return oidcSettings;
+};
 
 export const getOidcConfig = (oidcSettings) => {
   return objectAssign([
     defaultOidcConfig,
     snakeCasedSettings(oidcSettings),
-    { automaticSilentRenew: false } // automaticSilentRenew is handled in vuex and not by user manager
-  ])
-}
+    { automaticSilentRenew: false }, // automaticSilentRenew is handled in vuex and not by user manager
+  ]);
+};
 
-export const getOidcCallbackPath = (callbackUri, routeBase = '/') => {
+export const getOidcCallbackPath = (callbackUri, routeBase = "/") => {
   if (callbackUri) {
-    const domainStartsAt = '://'
+    const domainStartsAt = "://";
     const hostAndPath = callbackUri.substr(
       callbackUri.indexOf(domainStartsAt) + domainStartsAt.length
-    )
+    );
     return hostAndPath
       .substr(hostAndPath.indexOf(routeBase) + routeBase.length - 1)
-      .replace(/\/$/, '')
+      .replace(/\/$/, "");
   }
-  return null
-}
+  return null;
+};
 
 export const createOidcUserManager = (oidcSettings) => {
-  const oidcConfig = getOidcConfig(oidcSettings)
+  const oidcConfig = getOidcConfig(oidcSettings);
   requiredConfigProperties.forEach((requiredProperty) => {
     if (!oidcConfig[requiredProperty]) {
       throw new Error(
-        'Required oidc setting ' +
+        "Required oidc setting " +
           requiredProperty +
-          ' missing for creating UserManager'
-      )
+          " missing for creating UserManager"
+      );
     }
-  })
-  return new oidc.UserManager(oidcConfig)
-}
+  });
+  return new oidc.UserManager(oidcConfig);
+};
 
 export const addUserManagerEventListener = (
   oidcUserManager,
   eventName,
   eventListener
 ) => {
-  const addFnName = 'add' + firstLetterUppercase(eventName)
+  const addFnName = "add" + firstLetterUppercase(eventName);
   if (
-    typeof oidcUserManager.events[addFnName] === 'function' &&
-    typeof eventListener === 'function'
+    typeof oidcUserManager.events[addFnName] === "function" &&
+    typeof eventListener === "function"
   ) {
-    oidcUserManager.events[addFnName](eventListener)
+    oidcUserManager.events[addFnName](eventListener);
   }
-}
+};
 
 export const removeUserManagerEventListener = (
   oidcUserManager,
   eventName,
   eventListener
 ) => {
-  const removeFnName = 'remove' + firstLetterUppercase(eventName)
+  const removeFnName = "remove" + firstLetterUppercase(eventName);
   if (
-    typeof oidcUserManager.events[removeFnName] === 'function' &&
-    typeof eventListener === 'function'
+    typeof oidcUserManager.events[removeFnName] === "function" &&
+    typeof eventListener === "function"
   ) {
-    oidcUserManager.events[removeFnName](eventListener)
+    oidcUserManager.events[removeFnName](eventListener);
   }
-}
+};
 
 export const processSilentSignInCallback = () => {
-  new oidc.UserManager().signinSilentCallback()
-}
+  new oidc.UserManager().signinSilentCallback();
+};
 
 export const processSilentSignInCallbackWithFallback = (fallback) => {
   new oidc.UserManager().signinSilentCallback().catch((err) => fallback(err))
-}
+};
+
+export const processSilentSignInCallbackWithFallback = (fallback) => {
+  new oidc.UserManager().signinSilentCallback().catch((err) => fallback(err));
+};
 
 export const processSignInCallback = (oidcSettings) => {
   return new Promise((resolve, reject) => {
-    const oidcUserManager = createOidcUserManager(oidcSettings)
+    const oidcUserManager = createOidcUserManager(oidcSettings);
     oidcUserManager
       .signinRedirectCallback()
       .then((user) => {
-        resolve(sessionStorage.getItem('vuex_oidc_active_route') || '/')
+        resolve(sessionStorage.getItem("vuex_oidc_active_route") || "/");
       })
       .catch((err) => {
-        reject(err)
-      })
-  })
-}
+        reject(err);
+      });
+  });
+};
 
 export const tokenExp = (token) => {
   if (token) {
-    const parsed = parseJwt(token)
-    return parsed.exp ? parsed.exp * 1000 : null
+    const parsed = parseJwt(token);
+    return parsed.exp ? parsed.exp * 1000 : null;
   }
-  return null
-}
+  return null;
+};
 
 export const tokenIsExpired = (token) => {
-  const tokenExpiryTime = tokenExp(token)
+  const tokenExpiryTime = tokenExp(token);
   if (tokenExpiryTime) {
-    return tokenExpiryTime < new Date().getTime()
+    return tokenExpiryTime < new Date().getTime();
   }
-  return false
-}
+  return false;
+};
